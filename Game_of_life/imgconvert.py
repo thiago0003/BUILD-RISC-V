5d921b3e483de10c8cbc17ee69b8163e892c06bb
Guga <gbarreto@estudante.ufscar.br>
2023-10-24 23:30:38 -0300

    MArio is hexadecimal, not binary :D

--- Game_of_life/imgconvert.py
+++ Game_of_life/imgconvert.py
@@ -5,26 +5,26 @@ def read_mario8():
     with open('mario8.bin') as f:
         listing = []
         for line in f:
-            listing.append(line.strip())  
+            listing.append((line.strip()))  
     return listing
 
 
 row, col  = 20, 15
 
 l = read_mario8()
+
 n_l = np.zeros((2*row, 2*col))
 for i in range(row):
     for j in range(col):
         idx_y = 2 * i
         idx_x = 2 * j
-        n_l[idx_y  ,   idx_x] = l[col * i + j]
-        n_l[idx_y+1,   idx_x] = l[col * i + j]
-        n_l[idx_y  ,1 +idx_x] = l[col * i + j]
-        n_l[idx_y+1,1 +idx_x] = l[col * i + j]
+        n_l[idx_y  ,   idx_x] = int(l[col * i + j], 2)
+        n_l[idx_y+1,   idx_x] = int(l[col * i + j], 2)
+        n_l[idx_y  ,1 +idx_x] = int(l[col * i + j], 2)
+        n_l[idx_y+1,1 +idx_x] = int(l[col * i + j], 2)
 
-n_l = n_l.reshape((300,4))
+n_l = n_l.reshape((300,4)).astype(int)
 
-
-np.savetxt("mario8bit_exp.bin", n_l, fmt='%08i', delimiter="")
+np.savetxt("mario8bit_exp.bin", (n_l), fmt='%08X',delimiter="")
 
     
