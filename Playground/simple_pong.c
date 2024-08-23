41a7351a81b6d381496b4133001cb93a82f01a9d
gustav-vbl <gbarreto@estudante.ufscar.br>
2024-08-23 19:13:56 -0300

    just a lil paddle

--- Playground/simple_pong.c
+++ Playground/simple_pong.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+#include <termio.h>
+#include <sys/select.h>
+#include <fcntl.h>
 
 #define HEIGHT (30)
 #define WIDTH  (30)
@@ -11,8 +13,8 @@
 
 
 #define MIN(a,b) (((a)<(b))?(a):(b))
-#define for_x for (int x = 0; x < HEIGHT ; x++)
-#define for_y for (int y = 0; y < WIDTH  ; y++)
+#define for_x for (int x = 0; x < WIDTH ; x++)
+#define for_y for (int y = 0; y < HEIGHT  ; y++)
 
 // change later - dont use multiplication
 #define _at(x,y) ((x) + (y)*(WIDTH))
@@ -25,8 +27,6 @@ unsigned char visual_board[HEIGHT * WIDTH] = {0};
 unsigned char stack_layers[WIDTH * LAYERS] = {0}; 
 //for paddle
 unsigned char pos_x_paddle =  5;
-unsigned char pos_y_paddle =  5 + SIZE_PADDLE;
-
 
 //for ball
 unsigned char pos_x_ball = 8;
@@ -35,43 +35,118 @@ unsigned char pos_y_ball = HEIGHT >> 1;
 unsigned char prev_x,prev_y;
 unsigned char ball_dir = 1;
 
-// necessary ?
-unsigned char l_button = 0;
-unsigned char r_button = 0;
 
-void move_paddle();
-void update_v_ball();
-void update_v_paddle();
-void update_v_layers();
-void bounce();
+int read_button();
+
 
 void _sleep(unsigned int sec)
 {
-    // const int  cte = 25e3 * 2;
-    // int itr = sec * cte;
+    while(sec--);
 
-    // while(itr--);
-    usleep(1000);
 }
 
+#ifdef _DEBUG 
+// Function to check if a key has been pressed without blocking
+int kbhit() {
+    struct termios oldt, newt;
+    int oldf;
+    int ch;
+    fd_set readfds;
+    struct timeval tv;
 
-// IO input -- change later.
-void read_button()
-{  
-    unsigned char t = getchar();
-    switch (t)
+    // Set up the file descriptor set
+    FD_ZERO(&readfds);
+    FD_SET(STDIN_FILENO, &readfds);
+
+    // Set timeout to 0, so select() returns immediately
+    tv.tv_sec = 0;
+    tv.tv_usec = 0;
+
+    // Get the terminal settings for stdin
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+
+    // Disable canonical mode and echo
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+
+    // Set non-blocking mode on stdin
+    oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+
+    // Check if data is available
+    int result = select(STDIN_FILENO + 1, &readfds, NULL, NULL, &tv);
+
+    // Restore original terminal settings
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    fcntl(STDIN_FILENO, F_SETFL, oldf);
+
+    // Return 1 if key is pressed, 0 otherwise
+    return result > 0;
+}
+
+// Function to read a single character if available
+int read_button() {
+    if (kbhit()) {
+        char chr = getchar();
+
+        if(chr == 'a') return  1; 
+        if(chr == 'd') return -1;
+    }
+    return 0;  // Return -1 if no key was pressed
+}
+#else
+
+#define IO_BUTTON_1 (0x1) 
+#define IO_BUTTON_0 (0x2)
+#define IO_SEND     (0)
+#define IO_RECIVED  (1) 
+
+int IOs(int value, int addr, int IO_map);
+
+
+int IOs(int value, int addr, int IO_map)
+{
+    register int IO_RECIVED_REG asm("a6");
+    IO_RECIVED_REG = 0;
+
+    register int GPIO_addr asm("t0");
+
+    if(IO_map == IO_SEND)
     {
-    case 'a': {
-        l_button = 1;
-        break;
+        __asm__ (
+            "add %0, zero, zero\n" 
+            "addi %0, %0, 1\n" 
+            "slli %0, %0, 23\n"        
+            "sw %1, (t0)"
+            : "+r" (GPIO_addr)
+            : "r" (value)
+        );
     }
-    case 'd': {
-        r_button = 1;
-        break;
+    if(IO_map == IO_RECIVED)
+    {
+        __asm__ (
+            "add %0, zero, zero\n" 
+            "addi t0, t0, 1\n" 
+            "slli t0, t0, 23\n" 
+            "add t0, t0, %1 \n"
+            "lw a6, 0(t0)"
+            : "+r" (GPIO_addr)
+            : "r" (addr)
+        );
     }
-    default: l_button = r_button = 0;
-    }
+    return IO_RECIVED_REG;
 }
+
+
+int read_button() {
+    
+    // if both are pressed - 0, if not +/-1.
+    return IOs(0, IO_BUTTON_1, IO_RECIVED) - IOs(0, IO_BUTTON_0, IO_RECIVED);
+
+}
+
+#endif
 
 //only for debug
 void show()
@@ -84,54 +159,61 @@ void show()
 	fflush(stdout);
 }
 
-// // Alternative random - we must create a new one ?
-// unsigned char alt_rand(){
-//     return rand();
+void move_paddle()
+{
+    // read_button();
+    // neither pressed or not, return
+    int move = read_button();
+    
+    if(move == 0) goto update;
+
+    unsigned char res = pos_x_paddle - move;
+    
+    //bound check
+    pos_x_paddle = (0 <= res &&  res + SIZE_PADDLE < WIDTH) ? res : pos_x_paddle;
+    // pos_x_ball = 3;
+
+update:
+    // update to video.    
+    for_x visual_board[_at(x, HEIGHT - 2)] = (x >  pos_x_paddle-1) && 
+                                         (x < pos_x_paddle + SIZE_PADDLE+1); 
+
+}
+
+
+void move_ball()
+{
+
+
+
+
+}
+
+
+void game(){
+    move_paddle();
+    move_ball(); 
+    hit();
+    show();
+    usleep(100);
+}
+
+// void setup_game()
+// {
+//     for_x for(int l =0;l < LAYERS; l++)
+//         stack_layers[_at(x,l)] = 0;
+//     while (getchar())
+//     {
+//     bounce();
+     
+//     update_v_ball();
+//     update_v_layers();
+//     update_v_paddle();    
+//     show();
+//     }
 // }
 
 
-
-
-void game()
-{
-}
-
-void setup_game()
-{
-    for_x for(int l =0;l < LAYERS; l++)
-        stack_layers[_at(x,l)] = 0;
-    while (getchar())
-    {
-    bounce();
-     
-    update_v_ball();
-    update_v_layers();
-    update_v_paddle();    
-    show();
-    }
-}
-
-void update_v_layers(){
-    for(int l = 0; l < LAYERS;l++)
-    {
-        for_x {
-            visual_board[_at(x,l)] = stack_layers[_at(x,l)];
-        }
-    }
-}
-
-void hit()
-{
-    if(pos_y_ball >= LAYERS) return;
-
-    if(stack_layers[_at(pos_x_ball, pos_y_ball)] == 1) 
-        stack_layers[_at(pos_x_ball, pos_y_ball)] == 0;
-
-}
-
-
-// ball_dir -> direction of the ball (0=right, 1=left, 2=down, 3=up)
-// pos_x_ball, pos_y_ball -> current position of the ball
 void bounce()
 {
     // Move the ball based on its direction
@@ -218,41 +300,7 @@ void bounce()
     }
 }
 
-void update_v_ball()
-{
-    // //fix it
-    // print the ball
-    visual_board[_at(pos_x_ball, pos_y_ball)] = 1;
 
-}
-
-void update_v_paddle()
-{
-    for_x
-        visual_board[_at(x, HEIGHT-2)] = (x >  pos_x_paddle-1) && 
-                                         (x <= pos_x_paddle + SIZE_PADDLE+1);
-
-}
-
-void move_paddle()
-{
-    // read_button();
-    // neither pressed or not, return
-    if(r_button == l_button) goto update;
-    
-
-    //bound check
-    if(l_button && pos_x_paddle >= 0)
-        pos_x_paddle--;
-
-    //bound check
-    if(r_button && (pos_x_paddle + SIZE_PADDLE < WIDTH))
-        pos_x_paddle++;
-
-update:
-    //update position
-    update_v_paddle(); 
-}
 
 
 void ball()
@@ -263,12 +311,15 @@ void ball()
 int main()
 {
 
-    setup_game();
-    show();
-    getchar();
-    game();
 
-
+    // setup_game();
+    while(1) game();
+    // while (1)
+    // {
+    //     int i = read_button();
+    //     if(i!= 0) printf("button press %i\n", i);
+    //     usleep(1100);
+    // }
 
 
     return 0;
